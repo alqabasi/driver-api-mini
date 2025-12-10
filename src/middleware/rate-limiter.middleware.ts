6a51b929f83e95afcf617d01877b2ece27032fae
mahros.elqabasy <mahros.elqabasy@gmail.com>
2025-12-10 20:02:13 +0000

    rate-limiter middleware

--- src/middleware/rate-limiter.middleware.ts
+++ src/middleware/rate-limiter.middleware.ts
@@ -1,8 +1,12 @@
 import rateLimit from 'express-rate-limit';
 
+const WINDOW: number = 1; // minutes
+const MAX_REQUESTS: number = 5;
+
+
 export const rateLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 100 requests per windowMs
+  windowMs: WINDOW * 60 * 1000, // 1 minutes
+  max: MAX_REQUESTS, // Limit each IP to 100 requests per windowMs
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 });
